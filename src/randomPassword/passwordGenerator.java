5bc277b833b4c09f7b0f317d1daa637202a4a5a8
sufyali <54652850+sufyali@users.noreply.github.com>
2021-03-29 11:27:41 -0500

    adding intro message

--- src/randomPassword/passwordGenerator.java
+++ src/randomPassword/passwordGenerator.java
@@ -1,5 +1,10 @@
 package randomPassword;
+import java.util.Scanner;
 
 public class passwordGenerator {
-
+	
+	public  void printBeginningMessage() {
+		System.out.println("Welcome to You Shall Not Pass. This is a random password generator. To begin using the program, enter \"start\"."
+				+ "To end, enter \"quit\"");
+	}
 }
