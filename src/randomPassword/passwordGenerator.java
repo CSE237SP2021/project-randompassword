7306ee25ad5b2e0f6bd3666da1c6fed6e30d5cea
iarruza <User@172.16.62.111>
2021-04-13 14:19:48 -0500

    Implemented specified characters feature; almost final commit

--- src/randomPassword/passwordGenerator.java
+++ src/randomPassword/passwordGenerator.java
@@ -7,6 +7,7 @@ public class passwordGenerator {
 	private static String upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	private static String numbers = "1234567890";
 	private static String symbols = "~`!@#$%^&*()_-+={[}]|\\:;\"'<,>.?/";
+	private static String characters = lowerCase + upperCase + numbers + symbols;
 	
 	private static void printBeginningMessage() {
 		System.out.println("Welcome to You Shall Not Pass. This is a random password generator. "
@@ -19,17 +20,41 @@ public class passwordGenerator {
 		System.out.println("No command line argument specified or arguments were incorrect. Use help as a command line argument for more information on how to run this program.");
 		System.out.println("Example: java randomPassword.passwordGenerator help");
 	}
-	//TODO: Add specified characters feature to generating step
+	private static String addSpecifiedCharacters(String specifiedCharacters, String password) {
+		int randomIndices[] = new int[specifiedCharacters.length()];
+		for (int i = 0; i < randomIndices.length; i++) {
+			int index = new Random().nextInt(password.length());
+		}
+		String tempPassword = "";
+		for (int i = 0; i < specifiedCharacters.length(); i++) {
+			for (int j = 0; j < password.length(); j++) {
+				tempPassword += password.charAt(j);
+				if (randomIndices[i] == j) {
+					tempPassword += specifiedCharacters.charAt(i);
+				}
+			}
+			password = tempPassword;
+			tempPassword = "";
+		}
+		return password;
+	}
 	private static void generatePassword(String passwordLength) {
-		int length = Integer.parseInt(passwordLength);
-		
-		String firstIterationCharacters = lowerCase + upperCase + numbers + symbols; 
-		
+		int length = Integer.parseInt(passwordLength); 
 		String password = "";
 		for (int i = 0; i < length; i++) {
-			int index = new Random().nextInt(firstIterationCharacters.length());
-			password += firstIterationCharacters.charAt(index);
+			int index = new Random().nextInt(characters.length());
+			password += characters.charAt(index);
 		}
+		System.out.println(password);
+	}
+	private static void generatePassword(String passwordLength, String specifiedCharacters) {
+		int length = Integer.parseInt(passwordLength) - specifiedCharacters.length();
+		String password = "";
+		for (int i = 0; i < length; i++) {
+			int index = new Random().nextInt(characters.length());
+			password += characters.charAt(index);
+		}
+		password = addSpecifiedCharacters(specifiedCharacters, password);
 		System.out.println(password);
 	}
 	
@@ -46,7 +71,7 @@ public class passwordGenerator {
 					generatePassword(args[0]);
 				}
 			case 2:
-				//generatePassword(args[0], args[1]);
+				generatePassword(args[0], args[1]);
 		}
 	}
 }
