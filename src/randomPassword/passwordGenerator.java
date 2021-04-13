08a4dfac100f04bcac70775c46f60a61030fee70
iarruza <User@172.16.62.111>
2021-04-13 12:01:13 -0500

    Cleaned up code

--- src/randomPassword/passwordGenerator.java
+++ src/randomPassword/passwordGenerator.java
@@ -3,6 +3,31 @@ package randomPassword;
 import java.util.Random;
 
 public class passwordGenerator {
+	private static String lowerCase = "abcdefghijklmnopqrstuvwxyz";
+	private static String upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	private static String numbers = "1234567890";
+	private static String symbols = "~`!@#$%^&*()_-+={[}]|\\:;\"'<,>.?/";
+	
+	private static void printBeginningMessage() {
+		System.out.println("Welcome to You Shall Not Pass. This is a random password generator. "
+				+ "To begin using the program, run the program normally and add a password length as a command line argument.");
+		System.out.println("For example, if your desired password length is 5, run the program as: java randomPassword.passwordGenerator 5");
+	}
+	
+	private static void generatePassword(String passwordLength) {
+		int length = Integer.parseInt(passwordLength);
+		
+		String firstIterationCharacters = lowerCase + upperCase + numbers + symbols; 
+		int bankLength = firstIterationCharacters.length();
+		
+		String password = "";
+		for (int i = 0; i < length; i++) {
+			int index = new Random().nextInt(firstIterationCharacters.length());
+			password += firstIterationCharacters.charAt(index);
+		}
+		System.out.println(password);
+	}
+	
 	public static void main(String[] args) {
 		if (args.length == 0) {
 			System.out.println("No command line argument specified. Use help as a command line argument for more information on how to run this program.");
@@ -12,32 +37,7 @@ public class passwordGenerator {
 			printBeginningMessage();
 		}
 		else if (args.length > 0 && args[0].equals("help") == false) {
-			int passwordLength = Integer.parseInt(args[0]);
-			
-			//separated all possible characters for future iterations
-			//this will allow for the implementation of specific characters being needed
-			String lowerCase = "abcdefghijklmnopqrstuvwxyz";
-			String upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-			String numbers = "1234567890";
-			String symbols = "~`!@#$%^&*()_-+={[}]|\\:;\"'<,>.?/";
-			
-			//joined all strings for creation of random password function in first iteration **will change with user input implementation**
-			String firstIterationCharacters = lowerCase + upperCase + numbers + symbols; 
-			int bankLength = firstIterationCharacters.length();
-			
-			//password generating with length as specified by user
-			String password = "";
-			for (int i = 0; i < passwordLength; i++) {
-				int index = new Random().nextInt(firstIterationCharacters.length());
-				password += firstIterationCharacters.charAt(index);
-			}
-			System.out.println(password);
+			generatePassword(args[0]);
 		}
-	}
-	
-	private static void printBeginningMessage() {
-		System.out.println("Welcome to You Shall Not Pass. This is a random password generator. "
-				+ "To begin using the program, run the program normally and add a password length as a command line argument.");
-		System.out.println("For example, if your desired password length is 5, run the program as: java randomPassword.passwordGenerator 5");
 	}
 }
